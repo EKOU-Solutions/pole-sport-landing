790924e31827443bc1f0d288bfcbe73f588bea77
iTzKevinPG <kevinusechejimenez@gmail.com>
2025-12-27 18:33:08 -0500

    🧹 chore: clean unused pages and SEO config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://TODO_DOMINIO",
+  site: "https://polesport.ekou.lat",
   integrations: [react(), sitemap()],
 
   vite: {
